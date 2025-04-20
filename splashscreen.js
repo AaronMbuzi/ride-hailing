fb09feedae7793731266c503a09a8954912fb0eb
Aaron Mbuzi <86435544+AaronMbuzi@users.noreply.github.com>
2025-04-20 15:32:08 +0200

    init

---
+++ splashscreen.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+      const splash = document.getElementById('splash-screen');
+      splash.style.display = 'none';
+    }, 2500); // 3 seconds
+  });
